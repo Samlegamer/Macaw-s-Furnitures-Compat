d3cffa3b6c38ec1f92e190bc0d37d59933b4d56c
Samlegamer <samlegamer.yt@gmail.com>
2024-08-05 02:06:20 +0200

    1.19.2

--- 1.19.2/Forge/Macaw's Furnitures - BYG/src/main/java/fr/samlegamer/mcwfurnituresbyg/McwFurnituresBYG.java
+++ 1.19.2/Forge/Macaw's Furnitures - BYG/src/main/java/fr/samlegamer/mcwfurnituresbyg/McwFurnituresBYG.java
@@ -34,33 +34,61 @@ public class McwFurnituresBYG
 	private void ClientSetup(FMLClientSetupEvent event)
 	{
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.aspen_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_aspen_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.baobab_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_baobab_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.blue_enchanted_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_blue_enchanted_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.cherry_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_cherry_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.cika_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_cika_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.cypress_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_cypress_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.ebony_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_ebony_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.ether_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_ether_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.fir_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_fir_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.green_enchanted_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_green_enchanted_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.holly_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_holly_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.jacaranda_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_jacaranda_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.lament_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_lament_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.mahogany_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_mahogany_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.white_mangrove_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_white_mangrove_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.maple_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_maple_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.nightshade_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_nightshade_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.palm_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_palm_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.pine_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_pine_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.rainbow_eucalyptus_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_rainbow_eucalyptus_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.redwood_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_redwood_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.skyris_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_skyris_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.willow_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_willow_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.witch_hazel_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_witch_hazel_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.zelkova_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_zelkova_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.bulbis_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_bulbis_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.imparius_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_imparius_glass_table.get(), RenderType.translucent());
 		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.sythian_glass_table.get(), RenderType.translucent());
+		ItemBlockRenderTypes.setRenderLayer((Block)MFurniBYGBlocksRegistry.stripped_sythian_glass_table.get(), RenderType.translucent());
 	}
 	
 	public static class TabsMRBOP extends CreativeModeTab
